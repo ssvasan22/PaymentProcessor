802606f1a6a55f8b5d8d71bfe3d8bfe1b924ab6e
Srinivasan <suchitra_srinivasan@nuance.com>
2019-08-27 13:39:02 -0400

    eclipse junit fix

--- src/main/java/productCart/MemberPaymentInfo.java
+++
@@ -1,9 +0,0 @@
-package productCart;
-
-public interface MemberPaymentInfo {
-	public void productInfo(String name, String email_ID, String phone, String addr, boolean isPhysicalProduct);
-    public void bookInfo(String name, String email_ID, String phone, String addr, boolean isBook);
-    public void videoInfo(String name, String email_ID, String phone, String addr, boolean isVideo, String videoName);
-    public void memberActivate(String email_ID, boolean isActivate);
-    public void memberUpgrade(String email_ID, boolean isUpgrade);
-}
---
+++ src/test/java/PaymentTest.java
@@ -0,0 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import productCart.PaymentInfo;
+import services.Membership;
+
+public class PaymentTest {
+	ClassLoader classLoader = getClass().getClassLoader();
+	Membership member = new Membership();
+	PaymentInfo paymentInfo = new PaymentInfo(member);
+
+		@Before
+		public void setUp() {
+			member.setMember_name("Tom");
+			member.setEmailAddress("tom@ttt.ca");
+			member.setPhoneNumber("+1 123 456 7890");
+			member.setAddress("123 Boulevard St-Catherine, Montreal H12 N23, Quebec");
+		}
+
+		@Test
+		public void testProductBehaviour() {
+			paymentInfo.setProductName("Monitor");
+			paymentInfo.setPrice(220.00);
+//			paymentInfo.setPhysical_Product(false);
+			paymentInfo.setPhysical_Product(true);
+		}
+		
+		@Test
+		public void testBookBehaviour() {
+			paymentInfo.setBookName("Game Of Thrones");
+			paymentInfo.setPrice(130.00);
+			paymentInfo.setBook(true);
+		}
+		
+		@Test
+		public void testVideoBehaviour() {
+//			paymentInfo.setVideoName("Game Of Thrones");
+			paymentInfo.setVideoName("Learning to Ski");
+			paymentInfo.setVideo(true);
+		}
+		
+		@Test
+		public void testMembershipActivate() {
+			paymentInfo.setMembership(true);
+		}
+		
+		@Test
+		public void testMembershipUpgrade() {
+			paymentInfo.setMembershipUpgrade(true);
+		}
+
+}
